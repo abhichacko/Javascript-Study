87922c8e3e8eb2de6c2016d72f4ca16b87eef85c
unknown <itsmeabhilashthankachan@gmail.com>
2020-02-11 18:57:45 +0530

    arrow functions

---
+++ arrowfunctions.js
@@ -0,0 +1,14 @@
+let square = (a) => a * a;
+
+let n = 2;
+console.log(square(n));
+
+let display = (name) => {
+  console.log("inside arrow function", name);
+  //   return "hello";
+};
+
+let greeting = display("abhi");
+console.log(greeting);
+
+let sayHi = () => alert("Hello!");
